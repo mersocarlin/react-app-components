3d08626f19b8e4765a999d6f765adc4792625bd9
mersocarlin <mersocarlin@gmail.com>
2015-11-17 20:14:31 -0200

    Add webpack dependency

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+/* jshint node: true */
+var path = require('path');
+var webpack = require('webpack');
+
+
+module.exports = {
+  context: path.join(__dirname),
+  // entry: './lib/index.js',
+  //
+  // output: {
+  //   path: 'lib',
+  //   publicPath: 'lib/',
+  //   filename: 'index.js'
+  // },
+
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/lib',
+    filename: 'index.js',
+    library: 'react-app-components',
+    libraryTarget: 'umd'
+  },
+
+  // plugins: [
+  //   new webpack.HotModuleReplacementPlugin(),
+  //   new webpack.NoErrorsPlugin(),
+  //   new webpack.ProvidePlugin({
+  //     $: "jquery",
+  //     jQuery: "jquery",
+  //     "window.jQuery": "jquery",
+  //     "root.jQuery": "jquery"
+  //   })
+  // ],
+
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: /src/,
+        exclude: /node_modules/,
+        loader: 'babel'
+      },
+      { test: /\.css$/, loader: 'style-loader!css-loader' }
+      // {
+      //   test: /\.scss$/,
+      //   loader: 'style!css!sass?outputStyle=expanded&'
+      //   + 'includePaths[]=' + (path.resolve(__dirname, './bower_components'))
+      //   + '&'
+      //   + 'includePaths[]=' + (path.resolve(__dirname, './node_modules'))
+      // },
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   loader: 'imports?_=lodash&$=jquery!jsx?harmony!babel-loader'
+      // },
+      // {
+      //   test: /\.(png|jpg|jpeg|gif)$/,
+      //   loader: "file-loader"
+      // },
+      // {
+      //   test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      //   loader: "url-loader?limit=10000&minetype=application/font-woff"
+      // },
+      // {
+      //   test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      //   loader: "file-loader"
+      // }
+    ]
+  }
+};
