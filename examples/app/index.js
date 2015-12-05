e74f164961f1aedca4392aa8a64e22232b2cfc23
mersocarlin <mersocarlin@gmail.com>
2015-12-05 11:55:46 -0200

    Fix navigation with reload

--- examples/app/index.js
+++ examples/app/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Router } from 'react-router';
 import { render } from 'react-dom'
-import createBrowserHistory from 'history/lib/createBrowserHistory';
 
 
 import routes from './routes';
@@ -17,11 +16,8 @@ import './styles/app.scss';
 
 (() => {
 
-  let history = createBrowserHistory();
-
   document.addEventListener('DOMContentLoaded', () => {
-    render(
-      <Router history={history}>{routes}</Router>,
-      document.getElementById("main"));
+    render(routes, document.getElementById("main"));
   });
+
 })();
--- examples/app/routes.js
+++ examples/app/routes.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { IndexRoute, Router, Route } from 'react-router';
+import createHistory from 'history/lib/createHashHistory';
 
 
 import App from './containers/app';
 import Samples from './containers/samples';
 
 
+let history = createHistory({
+  queryKey: false
+});
+
+
 export default (
-  <Router>
+  <Router history={history}>
     <Route path="/" component={App}>
       <IndexRoute component={Samples} />
     </Route>
