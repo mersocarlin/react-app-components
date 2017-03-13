74d978edf25fdf89769e9b17f8ee127852d90b49
mersocarlin <mersocarlin@gmail.com>
2017-03-13 21:53:47 +0100

    Remove obsolete components

--- src/components/bootstrap/inputs/FormGroup.js
+++ src/components/bootstrap/inputs/FormGroup.js
@@ -1,14 +1,6 @@
 import React from 'react'
 
-/*
-<div class="form-group">
-  <label for="exampleInputEmail1">Email address</label>
-  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-</div>
-*/
-
-const FormGroup = ({ label, input }) => (
+const FormGroup = ({ input, label }) => (
   <div className="form-group">
     {label}
     {input}
