2338ef2a3f5defb67697f791b0b60bc716949c60
mersocarlin <mersocarlin@gmail.com>
2017-03-14 19:25:13 +0100

    Render Button story with only one category

--- stories/components/bootstrap/Button.js
+++ stories/components/bootstrap/Button.js
@@ -6,35 +6,19 @@ import { Button, Icon } from '../../../src'
 export default () => {
   storiesOf('Bootstrap > Button', module)
     .add('Default', () => (
-      <Button>Click-me!</Button>
-    ))
-    .add('Primary', () => (
-      <Button asButton primary>Click-me!</Button>
-    ))
-    .add('Secondary', () => (
-      <Button asButton secondary>Click-me!</Button>
-    ))
-    .add('Success', () => (
-      <Button asButton success>Click-me!</Button>
-    ))
-    .add('Info', () => (
-      <Button asButton info>Click-me!</Button>
-    ))
-    .add('Warning', () => (
-      <Button asButton warning>Click-me!</Button>
-    ))
-    .add('Danger', () => (
-      <Button asButton danger>Click-me!</Button>
-    ))
-    .add('Link', () => (
-      <Button asButton link>Click-me!</Button>
-    ))
-    .add('Disabled', () => (
-      <Button asButton primary disabled>Click-me!</Button>
-    ))
-    .add('with Icon', () => (
-      <Button asButton primary>
-        <Icon icon="hand-spock-o" /> Click-me!
-      </Button>
+      <div>
+        <Button>Default</Button><br />
+        <Button asButton primary>Primary</Button><br />
+        <Button asButton secondary>Secondary</Button><br />
+        <Button asButton success>Success</Button><br />
+        <Button asButton info>Info</Button><br />
+        <Button asButton warning>Warning</Button><br />
+        <Button asButton danger>Danger</Button><br />
+        <Button asButton link>Link</Button><br />
+        <Button asButton primary disabled>Disabled</Button><br />
+        <Button asButton primary>
+          <Icon icon="hand-spock-o" /> With Icon
+        </Button><br />
+      </div>
     ))
 }
